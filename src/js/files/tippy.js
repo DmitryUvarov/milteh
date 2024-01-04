4d1836b86b9d887064030c6476c68f946c178cea
Dima <uvarov3123@gmail.com>
2024-01-04 13:33:23 +0200

    add hero

--- src/js/files/tippy.js
+++ src/js/files/tippy.js
@@ -1,17 +1,28 @@
 // Підключення функціоналу "Чертоги Фрілансера"
-import { isMobile, FLS } from "./functions.js";
+import { isMobile, FLS } from "./functions.js"
 // Підключення списку активних модулів
-import { flsModules } from "./modules.js";
+import { flsModules } from "./modules.js"
 
 // Підключення з node_modules
-import tippy from 'tippy.js';
+import tippy from "tippy.js"
 
 // Підключення стилів з src/scss/libs
-import "../../scss/libs/tippy.scss";
+import "../../scss/libs/tippy.scss"
 // Підключення стилів з node_modules
 //import 'tippy.js/dist/tippy.css';
 
 // Запускаємо та додаємо в об'єкт модулів
-flsModules.tippy = tippy('[data-tippy-content]', {
+flsModules.tippy = tippy("[data-tippy]", {
+  content(reference) {
+    let theme = reference.getAttribute("data-tippy") // dark
+    let title = reference.getAttribute("data-tippy-title")
+    let text = reference.getAttribute("data-tippy-text")
 
-});
+    return `<div class="tippy-block ${theme ? "tippy-block_dark" : ""}">
+                <h6 class="tippy-block__title">${title}</h6>
+                <p class="tippy-block__text">${text}</p>
+            </div>`
+  },
+  //   content: "Tooltip",
+  allowHTML: true,
+})
