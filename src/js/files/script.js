59bc6def48a70d4ef94f55562cb663a441e5b67e
Dima <uvarov3123@gmail.com>
2024-01-04 23:11:02 +0200

    updates inputs

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,7 +1,7 @@
 // Підключення функціоналу "Чертоги Фрілансера"
-import { isMobile } from "./functions.js";
+import { isMobile } from "./functions.js"
 // Підключення списку активних модулів
-import { flsModules } from "./modules.js";
+import { flsModules } from "./modules.js"
 
 // window.addEventListener('load', pageLoad)
 
